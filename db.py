485f52313f6fac94ac711687d46d2567eb9f4b2e
Pia Ruusukivi <ruusukivi@gmail.com>
2021-09-18 17:41:24 +0300

    Alternative database fix for Heroku

--- db.py
+++ db.py
@@ -3,5 +3,5 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 # Replace hack done for Heroku due to a change in the sqlalchemy library
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql:///", 1)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
